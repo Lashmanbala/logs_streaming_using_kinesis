e91dfa587558fb7c6e16a3d68c6534b43b182d36
lashmanbala <lashmanbala@gmail.com>
2024-09-25 12:53:17 +0530

    Comments and print messages

--- firehose_delivery_stream/app.py
+++ firehose_delivery_stream/app.py
@@ -5,17 +5,19 @@ def deploy():
     bucket_name = 'user-logs-bucket-1'
 
     s3_bucket_arn = create_bucket(bucket_name)
-    print(s3_bucket_arn)
+    print(f's3 bucket created successfully: {s3_bucket_arn}')
 
     role_arn = create_iam_role(s3_bucket_name = bucket_name)
-    print(role_arn)
+    print(f'IAM role created successfully: {role_arn}')
 
     delivery_stream_name = 'user-logs-s3-stream'
+    # This s3 prefix is to create folder structure like partitioned data inorder to create catelog table.
     S3_path_prefix = 'logs/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}/hour=!{timestamp:HH}/'
     buffer_size = 1
     buffer_time = 10
 
     res = create_delivery_stream(delivery_stream_name, s3_bucket_arn, role_arn, S3_path_prefix, buffer_size, buffer_time)
+    print('Firehose delivery stream created successfully')
     print(res)
 
 if __name__ == "__main__":
